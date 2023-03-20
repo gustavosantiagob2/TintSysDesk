d5210e95637f9b1d9e6a16fdbd0867794aad4539
gustavosantiagob2 <gustavohenriquesantiago5@gmail.com>
2023-03-20 17:31:12 -0300

    Adicionar arquivos de projeto.

---
+++ TintSysClass/Banco.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace TintSysClass
+
+{
+    internal static class Banco
+    {
+        private static string strConn;
+
+       public static MySqlCommand Abir()
+        {
+            strConn =@"server =127.0.0.1;database =ti93sysdb; port =3306; user d =root; passowrd =usbw";
+           MySqlCommand cmd = new MySqlCommand();
+
+            try // Tentar abrir
+            {
+                MySqlConnection cn = new MySqlConnection(strConn);
+                if (cn.State != System.Data.ConnectionState.Open )
+                {
+                    cn.Open();
+                }
+                cmd.Connection = cn;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return cmd;
+        }
+
+    }
+}
